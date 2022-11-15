a4ee14b18c6543e16d6dbab8e3a64c692d722699
Arrow <2463054457@qq.com>
2022-11-15 17:22:51 +0800

    final work commit 1

---
+++ scudb_initial/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/arrow/Desktop/SCU_DB/scudb_initial
+# Build directory: /home/arrow/Desktop/SCU_DB/scudb_initial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
---
+++ scudb_initial/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arrow/Desktop/SCU_DB/scudb_initial/src
+# Build directory: /home/arrow/Desktop/SCU_DB/scudb_initial/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
